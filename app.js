6c14347b2af9a242996babca59a435e9ce9a0780
tthanat91 <118584958+tthanat91@users.noreply.github.com>
2026-08-31 21:11:14 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -47,7 +47,7 @@
 
   /* Shown in Settings. If this is not the newest value, the browser is
      serving a cached copy of app.js — bump the ?v= tokens in index.html. */
-  var APP_BUILD = '2026-08-02bv';
+  var APP_BUILD = '2026-08-02bw';
 
   var prefs = Object.assign({}, DEFAULT_PREFS, readJSON(LS.prefs, {}));
   var scriptUrl = localStorage.getItem(LS.url) || SITE.scriptUrl || '';
