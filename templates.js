9023e1e6451811d84af7536f7c81ac48e4dc62b3
tthanat91 <118584958+tthanat91@users.noreply.github.com>
2026-09-08 19:37:25 +0700

    Add files via upload

--- templates.js
+++ templates.js
@@ -33,7 +33,7 @@
   var TEAM = 'ทีมผ่าตัด | Operative team';
   var DIAG = 'การวินิจฉัยและหัตถการ | Diagnosis & procedure';
 
-  global.TEMPLATES_BUILD = '2026-08-02dz';
+  global.TEMPLATES_BUILD = '2026-08-02ea';
 
   global.DEFAULT_TEMPLATES = [
 
